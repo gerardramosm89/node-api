459921f5eeac381a233fe0163be3893f31e88537
root <gerard.ramosm89@gmail.com>
2017-01-04 12:50:12 -0800

    Get and Post refactored

--- server.js
+++ server.js
@@ -4,91 +4,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-let contacts = require('./data');
-app.get('/api/contacts', (request, response) => {
-  if (!contacts) {
-    response.status(404).json({ message: 'No contacts found.' });
-  }
-  response.json(contacts);
-});
+mongoose.connect('mongodb://localhost:27017/myStuff');
 
-app.get('/api/contacts/:id', (request, response) => {
 
-  let contactId = request.params.id;
-
-  let contact = contacts.filter(contact => {
-		if (contact.id == contactId) {
-			contactId = contact;
-			return contactId
-		};
-  });
-
-  if (!contact) {
-    response.status(404).json({ message: 'Contact not found' });
-  }
-
-  response.json(contact[0]);
-});
-
-app.post('/api/contacts', (request, response) => {
-
-  let contact = {
-    id: contacts.length + 1,
-    first_name: request.body.first_name,
-    last_name: request.body.last_name,
-    email: request.body.email,
-    website: request.body.website
-  };
-
-  contacts.push(contact);
-
-  response.json(contact);
-
-});
-
-app.put('/api/contacts/:id', (request, response) => {
-
-  let contactId = request.params.id;
-
-  let contact = contacts.filter(contact => {
-    return contact.id == contactId;
-	})[0];
-	const index = contacts.indexOf(contact);
-
-  let keys = Object.keys(request.body);
-
-  keys.forEach(key => {
-    contact[key] = request.body[key];
-  });
-
-  contacts[index] = contact;
-	console.log("The index is: ", index);
-  // response.json({ message: `User ${contactId} updated.`});
-	response.json(contacts[index]);
-});
-
-app.delete('/api/contacts/:id', (request, response) => {
-  
-  let contactId = request.params.id;
-	if (contactId > contacts.length + 1) {
-		return response.json("Not a valid ID, user doesn't exist");
-	} else {
-  let contact = contacts.filter(contact => {
-    return contact.id == contactId;
-  })[0];
-
-  const index = contacts.indexOf(contact);
-
-  contacts.splice(index, 1);
-  response.json({ message: `User ${contactId} deleted.`});
-	}
-});
-
+app.use('/api/contacts', require('./api/contacts/routes/post_contact'));
+app.use('/api/contacts', require('./api/contacts/routes/get_contacts'));
 const hostname = 'localhost';
 const port = 3001;
 app.get('*', function(req, res) {
